f39dc366b7d03e9d5b13fcd65bcabb2d25556c00
Eric <eric.personblack@gmail.com>
2026-02-13 00:02:29 -0300

    criação de um arquivo com instruções git e acrescentando código de JS

--- Ada/JS/AULA-02/while.js
+++ Ada/JS/AULA-02/while.js
@@ -23,3 +23,45 @@ const prompt = require("readline-sync");
 
 // console.log("A média das notas é: ", somaNotas/quantNotasValidas);
 
+// let qntTentativas = 0;
+// const numeroAleatorio = parseInt(Math.random() * 10);
+
+// let numeroUsuario = Number(prompt.question("Adivinhe o número que estou pensando (entre 0 e 10): "));
+
+// while (numeroUsuario !== numeroAleatorio){
+//     console.log("Número incorreto! Tente novamente.");
+//     numeroUsuario = Number(prompt.question("Adivinhe o número que estou pensando (entre 0 e 10): "));
+//     qntTentativas += 1;
+// }
+
+// console.log("Parabéns! Você adivinhou o número correto:", numeroAleatorio);
+// console.log("Número de tentativas:", qntTentativas);
+
+// let contador = 0;
+
+// while (contador < 5){
+//     console.log("Contador é igual a: ", contador);
+//     contador ++;
+
+//     //if (contador === 2){
+//        // break;
+//     //}
+
+//     if (contador === 2){
+//         continue;
+//     }
+
+//     console.log("depois do if:...");
+// }
+
+
+
+// Do-while - iniciar pelo menos uma vez aquele bloco de código
+
+let saldo;
+
+do {
+    saldo = Number(prompt.question("Qual o seu saldo? "));
+} while (saldo < 0);
+
+console.log(saldo);

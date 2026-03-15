19000914ae90daabb99ad98d27739487fe64cf27
Eric <eric.personblack@gmail.com>
2026-03-14 22:35:09 -0300

    .

---
+++ Ada/API/aula-1/js/script.js
@@ -0,0 +1,29 @@
+// Fetch API
+
+// fetch("https://viacep.com.br/ws/01001000/json/")
+//   .then((response) => {
+//     console.log(response);
+//     response.json().then((dados) => console.log(dados))
+//   })
+//   .catch((erro) => {
+//     console.log(erro);
+//   });
+
+// async function obterDadosDoCep() {
+//   try {
+//     const response = await fetch("https://viacep.com.br/ws/01001000/json/");
+//     const dados = await response.json();
+
+//     console.log(dados);
+//   } catch (erro) {
+
+//     console.log(erro);
+//   } finally {
+
+//     console.log("acabou a requisição");
+//   }
+// }
+
+// obterDadosDoCep();
+
+

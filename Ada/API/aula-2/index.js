eaa9e4c415f98951b58cdc27ed843f033d492f5f
Eric <eric.personblack@gmail.com>
2026-03-15 11:28:20 -0300

    adicionando info. de ts

--- Ada/API/aula-2/index.js
+++ Ada/API/aula-2/index.js
@@ -1,8 +1,24 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const n1 = 5;
-const n2 = 4;
-let valor;
-valor = 5;
-console.log(n1 + valor);
+const pessoa = {
+    nome: "Eric",
+    idade: 23,
+    profissao: "Dev",
+    altura: 1.68,
+};
+console.log(pessoa);
+function escolherNumero(numero1, numero2, criterio) {
+    switch (criterio) {
+        case 'greater':
+            return numero1 > numero2 ? numero1 : numero2;
+        case 'lower':
+            return numero1 < numero2 ? numero1 : numero2;
+        default:
+            const numeroAleatorio = Math.random();
+            if (numeroAleatorio >= 0.5)
+                return numero1;
+            return numero2;
+    }
+}
+const escolhido = escolherNumero(1, 20, "lower");
+console.log(escolhido);
 //# sourceMappingURL=index.js.map
--- Ada/API/aula-2/index.ts
+++ Ada/API/aula-2/index.ts
@@ -1,7 +1,44 @@
-const n1 = 5;
-const n2 = 4;
+interface Person {
+  nome: string;
+  idade: number;
+  profissao?: string;
+  altura: number;
+}
 
-let valor: number;
-valor = 5;
+const pessoa: Person = {
+  nome: "Eric",
+  idade: 23,
+  profissao: "Dev",
+  altura: 1.68,
+};
 
-console.log(n1 + valor);
+console.log(pessoa)
+
+// criando tipagem com type
+
+// type Person = {
+//   nome: string;
+//   idade: number;
+//   profissao?: string;
+//   altura: number;
+// };
+
+type Criterio = 'greater'|'lower'
+
+function escolherNumero (numero1: number, numero2: number, criterio?: Criterio): number{
+    switch(criterio){
+        case 'greater':
+            return numero1 > numero2 ? numero1 : numero2
+        case 'lower':
+            return numero1 < numero2 ? numero1 : numero2
+        default:
+            const numeroAleatorio = Math.random();
+
+            if (numeroAleatorio >= 0.5) return numero1; 
+            return numero2;
+    }
+}
+
+const escolhido = escolherNumero(1, 20, "lower");
+
+console.log(escolhido);

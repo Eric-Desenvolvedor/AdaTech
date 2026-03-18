8f9e06584eddd0839d35f5ffe906400eab463471
Eric <eric.personblack@gmail.com>
2026-03-17 21:35:37 -0300

    Adicionando um projeto em React sobre Notícias

---
+++ Ada/React/primeiro-projeto/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "../../assets/images/brandIcon.png";
+import './styles.css'
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <header>
+        <nav id="navbar">
+          <div className="nav-brand">
+            <img src={logo} alt="Logo" />
+            <h1>Space Flight News</h1>
+          </div>
+
+          <ul className="nav-list">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Trending</a>
+            </li>
+            <li>
+              <a href="/">Categories</a>
+            </li>
+            <li>
+              <a href="/">About us</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Navbar;
